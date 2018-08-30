ddd92e78846290f68c6f1ba70a489f5a1a12be09
Joe Mahon <joe.mahon@thelevelup.com>
2018-08-30 18:02:58 -0400

    Simplify

--- app/src/main/java/com/thelevelup/rosegarden/GardenActivity.java
+++ app/src/main/java/com/thelevelup/rosegarden/GardenActivity.java
@@ -2,12 +2,10 @@ package com.thelevelup.rosegarden;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 public class GardenActivity extends Activity {
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden);
     }
